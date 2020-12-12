7066f1608f9ecc3ac8f5eb5fa2b328f030f442f1
rigma <rigbuntu@gmail.com>
2020-12-12 17:48:46 +0100

    feat: add default layout for the website

---
+++ components/Navbar.vue
@@ -0,0 +1,68 @@
+<template>
+  <nav>
+    <h1 class="title">
+      <nuxt-link to="/">
+        rigma's lair
+      </nuxt-link>
+    </h1>
+    <ul class="links">
+      <li>
+        <nuxt-link to="/">
+          Home
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/resume">
+          Resume
+        </nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+@import '~/assets/css/colors.css';
+
+nav {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 16px 12px;
+}
+
+nav a {
+  color: var(--link-color);
+  text-decoration: none;
+}
+
+.title {
+  margin-right: 16px;
+}
+
+.title a {
+  color: var(--title-color);
+  text-decoration: none;
+}
+
+.links {
+  display: inline-flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  margin-top: 8px;
+  padding: 0;
+}
+
+.links li {
+  list-style: none;
+  padding: 0 8px;
+}
+
+
+.links a {
+  text-decoration: none;
+}
+
+.links a.nuxt-link-exact-active {
+  text-decoration: underline dotted;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container">
+    <Navbar />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Navbar from '@/components/Navbar.vue'
+
+export default Vue.extend({
+  components: {
+    Navbar
+  }
+})
+</script>
+
+<style>
+@import '~/assets/css/colors.css';
+
+html {
+  background-color: var(--background-color);
+  font-family: 'Gelasio', 'Times New Roman', Times, serif;
+  font-size: 16px;
+  color: var(--text-color);
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-family: 'Lato', Helvetica, Arial, sans-serif;
+  color: var(--title-color);
+}
+
+h1 {
+  font-size: 2em;
+}
+
+h2 {
+  font-size: 1.6em
+}
+
+h3 {
+  font-size: 1.4em;
+}
+
+.container {
+  min-height: 100vh;
+  margin: 0 auto;
+}
+
+@media (min-width: 480px) {
+  .container {
+    min-width: 480px;
+  }
+}
+
+@media (min-width: 768px) {
+  .container {
+    max-width: 768px;
+  }
+}
+
+@media (min-width: 1024px) {
+  .container {
+    max-width: 1024px;
+  }
+}
+</style>
