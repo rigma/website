0dff9c24315f6474f35c66788a8692b25ca0ae6b
rigma <rigbuntu@gmail.com>
2020-12-13 12:23:50 +0100

    feat: add wip resume page

---
+++ pages/resume.vue
@@ -0,0 +1,15 @@
+<template>
+  <article>
+    🛠🧑‍💻 <span class="italic">Work in progress…</span>
+  </article>
+</template>
+
+<style scoped>
+article {
+  text-align: center;
+}
+
+.italic {
+  font-style: italic;
+}
+</style>
