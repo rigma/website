71a890db75854cba5db6ac3cf3e529f581c601f3
rigma <rigbuntu@gmail.com>
2020-12-15 13:22:15 +0100

    feat: extracting wip elements into a component

---
+++ components/WorkInProgress.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    🛠🧑‍💻 <span class="italic">{{ $t('message') }}</span>
+  </section>
+</template>
+
+<i18n>
+{
+  "en": {
+    "message": "Work in progress…"
+  },
+  "fr": {
+    "message": "Développement en cours…"
+  }
+}
+</i18n>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'WorkInProgress'
+})
+</script>
+
+<style scoped>
+section {
+  text-align: center;
+}
+
+.italic {
+  font-style: italic;
+}
+</style>
