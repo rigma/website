85dc77e22ab7c949d2dc482e56e654fff9f26f67
rigma <rigbuntu@gmail.com>
2020-12-15 17:46:44 +0100

    feat: add some style to footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,12 @@
 <template>
   <footer>
-    <i18n path="note" tag="p">
-      <a href="https://nuxtjs.org" target="_blank">{{ $t('nuxt') }}</a>
-    </i18n>
-    <LocaleSelector />
+    <hr />
+    <div class="layout">
+      <i18n path="note" tag="p" class="note">
+        <a href="https://nuxtjs.org" target="_blank">{{ $t('nuxt') }}</a>
+      </i18n>
+      <LocaleSelector />
+    </div>
   </footer>
 </template>
 
@@ -35,9 +38,41 @@ export default Vue.extend({
 <style scoped>
 footer {
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+
+hr {
+  border: none;
+  border-top: solid 1px rgba(148, 148, 148, .4);
+  width: 50%;
+}
+
+.layout {
+  display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
   padding: 8px 12px;
+  width: 100%;
+}
+
+.note {
+  color: #777;
+  font-size: .75em;
+  font-style: italic;
+}
+
+@media (min-width: 480px) {
+  hr {
+    width: calc(.75 * 50%);
+  }
+}
+
+@media (min-width: 768px) {
+  hr {
+    width: calc(50% / 2);
+  }
 }
 </style>
