a3a179489f68738909ceb8888358d1baaf7a5a90
rigma <rigbuntu@gmail.com>
2021-01-01 18:14:45 +0100

    chore: importing used icons in library

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,10 +1,43 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faBug, faCode, faTerminal } from '@fortawesome/free-solid-svg-icons'
+import { faBug, faCode, faDatabase, faTerminal } from '@fortawesome/free-solid-svg-icons'
+import {
+  faAws,
+  faDocker,
+  faGitAlt,
+  faGithub,
+  faJira,
+  faJs,
+  faLaravel,
+  faPython,
+  faNode,
+  faReact,
+  faRust,
+  faTrello,
+  faVuejs,
+} from '@fortawesome/free-brands-svg-icons'
 import Vue from 'vue'
 
 config.autoAddCss = false
-library.add(faBug, faCode, faTerminal)
+library.add(
+  faAws,
+  faBug,
+  faCode,
+  faDatabase,
+  faDocker,
+  faGitAlt,
+  faGithub,
+  faJira,
+  faJs,
+  faLaravel,
+  faPython,
+  faNode,
+  faReact,
+  faRust,
+  faTerminal,
+  faTrello,
+  faVuejs
+)
 
 Vue.component('FontAwesomeIcon', FontAwesomeIcon)
 
