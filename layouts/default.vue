a063e8ae22d98dd7da78a6283d7acd772175f37f
rigma <rigbuntu@gmail.com>
2021-01-01 17:23:07 +0100

    fix: removing duplicate rules from layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -34,14 +34,6 @@ export default {
 
 html {
   background-color: var(--background-color);
-  font-family: 'Gelasio', 'Times New Roman', Times, serif;
-  font-size: 16px;
-  color: var(--text-color);
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
 }
 
